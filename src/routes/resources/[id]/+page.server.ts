282293eeb2868257c1491c820a9bf0035ebb861d
A.R.Z <lexz.zaldivar@gmail.com>
2022-10-19 06:17:06 -0400

    Update +page.server.ts

--- src/routes/resources/[id]/+page.server.ts
+++ src/routes/resources/[id]/+page.server.ts
@@ -1,19 +1,18 @@
 import { API_TOKEN } from "$env/static/private";
-import { PUBLIC_API_URL } from '$env/static/public';
+import { get } from "$lib/api";
 import type { Response } from "$lib/types/data";
 import type { Resource } from "$lib/types/resources";
 import { error } from "@sveltejs/kit";
 
 export async function load({ params }: { [key: string]: any }): Promise<Response<Resource>> {
 	try {
-		const response = await fetch(`${PUBLIC_API_URL}/api/wc-resources/${params.id}?populate=*`, {
-			method: 'GET',
-			headers: {
-				'Authorization': `Bearer ${API_TOKEN}`
-			}
-		});
-		const data = await response.json();
-		return data;
+
+		const q = {
+			populate: '*'
+		}
+
+		const detail = await get(`wc-resources/${params.id}`, q);
+		return detail;
 	} catch (e) {
 		throw error(500, `Error ${e}`);
 	}
