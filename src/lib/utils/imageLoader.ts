0122ee06a306807495635c13b255199bc06c8971
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-06-07 19:31:44 -0700

    Fixes

--- src/lib/utils/imageLoader.ts
+++ src/lib/utils/imageLoader.ts
@@ -5,8 +5,6 @@ type ImageLoaderOptions = {
 }
 
 export default function imageLoader({src, width, quality}: ImageLoaderOptions): string {
-    // REMOVE
-    if (!src) return 'https://placehold.co/600x400';
     const imageUrl = src.split('/');
 	let imageId;
 	if (src.indexOf('imagedelivery.net') !== -1) {
@@ -14,6 +12,12 @@ export default function imageLoader({src, width, quality}: ImageLoaderOptions): 
 	} else {
 		imageId = imageUrl[imageUrl.length - 1];
 	}
+
+	// Temp fixes
+	if (imageId.startsWith('thumbnail') || imageId.startsWith('large') || imageId.indexOf('.gif') !== -1) {
+		return `https://api.transparency-initiative.org/uploads/${imageId}`;
+	}
+
 	const params = [];
 	if (width) params.push(`width=${width}`);
 	if (quality) params.push(`quality=${quality}`);
