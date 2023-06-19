ab90d098d2df0b0557fd4d6216c0633527414bb5
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-06-19 00:07:11 -0700

    PDF Previews

--- src/lib/utils/pdfjs.ts
+++ src/lib/utils/pdfjs.ts
@@ -1,43 +1,28 @@
-const initPdfjs = async () => {
-    const pdfjs = await import('pdfjs-dist/build/pdf');
-    pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
-    return pdfjs;
-}
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
-// render the viewer
-// Path: src/lib/utils/pdfjs.ts
-const renderPdf = async (pdfjs: any, pdfUrl: string, container: HTMLElement) => {
-
-    const data = await fetch('/api/file?url=' + pdfUrl, {
-        cache: 'force-cache'
-    }).then(res => res.arrayBuffer());
-    
-    const pdf = await pdfjs.getDocument({data}).promise;
-    const page = await pdf.getPage(1);
-    const viewport = page.getViewport({ scale: 1 });
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-    container.appendChild(canvas);
-    await page.render({ canvasContext: context, viewport }).promise;
-}
-
-export const renderPdfViewer = async (container: string) => {
-
-    const elems = document.querySelectorAll(container);
-    if (elems.length === 0) {
-        return;
-    }
-
-    for (const elem of elems) {
-
-        const pdfUrl = elem.getAttribute('data-pdf-url');
-        if (!pdfUrl) {
-            continue;
-        }
-
-        const pdfjs = await initPdfjs();
-        await renderPdf(pdfjs, pdfUrl, elem as HTMLElement);
+export const renderPdfPreview = async (pdfUrl: string) => {
+    try {
+        const Canvas = require('canvas');
+        const pdfjsLib = require('pdfjs-dist');
+        const data = await fetch(pdfUrl, {
+            cache: 'force-cache'
+        }).then(res => res.arrayBuffer());
+        const pdfDoc = await pdfjsLib.getDocument({
+            data
+        }).promise;
+        const page = await pdfDoc.getPage(1);
+        const viewport = page.getViewport({ scale: 1 });
+        const canvas = Canvas.createCanvas(viewport.width, viewport.height);
+        const renderContext = {
+            canvasContext: canvas.getContext('2d'),
+            viewport
+        };
+        await page.render(renderContext).promise;
+        const dataUrl = canvas.toDataURL();
+        return dataUrl;
+    } catch (err) {
+        console.log(err);
+        return null;
     }
 }
