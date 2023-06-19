9aad09a43d6ea72f76fd5db1e3726c5c7c16a49a
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-06-19 04:33:22 -0700

    Update pdfjs.ts

--- src/lib/utils/pdfjs.ts
+++ src/lib/utils/pdfjs.ts
@@ -1,7 +1,31 @@
 import { createCanvas } from 'canvas';
 
+import pdfjsLib from 'pdfjs-dist';
+
 
 export const generatePdfPreview = async (url: string) => {
-  const canvas = createCanvas(100, 100);
+  
+  const data = await fetch(url, {
+    cache: 'force-cache'
+  }).then(res => res.arrayBuffer());
+
+  const pdf = await pdfjsLib.getDocument({
+    data
+  }).promise;
+
+  const page = await pdf.getPage(1);
+
+  const viewport = page.getViewport({
+    scale: 1
+  });
+
+  const canvas = createCanvas(viewport.width, viewport.height);
+  const context = canvas.getContext('2d');
+
+  await page.render({
+    canvasContext: context as any,
+    viewport
+  }).promise;
+
   return canvas.toDataURL();
 }
