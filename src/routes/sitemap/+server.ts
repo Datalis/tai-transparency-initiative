349264356677cba884aee9e29eb8ce41bac6589e
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-06-19 20:53:47 -0700

    Add sitemap support

---
+++ src/routes/sitemap/+server.ts
@@ -0,0 +1,9 @@
+import { API_SERVER } from "$env/static/private";
+
+export const GET = async () => {
+	const fileUrl = `${API_SERVER}/api/sitemap/get`;
+	const data = await fetch(fileUrl, {
+        cache: "no-cache"
+    }).then((res) => res.arrayBuffer());
+	return new Response(data);
+};
