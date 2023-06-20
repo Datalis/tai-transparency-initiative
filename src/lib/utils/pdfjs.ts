beba8e2fc48a85fc1c552c9b333cf9a9b883ac11
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2023-06-19 20:23:57 -0700

    Update pdfjs.ts

--- src/lib/utils/pdfjs.ts
+++ src/lib/utils/pdfjs.ts
@@ -1,16 +1,8 @@
 import { createCanvas } from 'canvas';
 import pdfjsLib from 'pdfjs-dist';
 
-if (process.env.NETLIFY) {
-	// Workaround for Netlify. TODO: Remove
-	pdfjsLib.GlobalWorkerOptions.workerSrc = '/var/task/node_modules/.pnpm/pdfjs-dist@3.7.107/node_modules/pdfjs-dist/build/pdf.worker.js';
-}
-
 export const generatePdfPreview = async (url: string) => {
 	try {
-		
-		
-
 		const data = await fetch(url, {
 			cache: 'force-cache'
 		}).then((res) => res.arrayBuffer());
